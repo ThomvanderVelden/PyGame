aa5e4c114b921af64485c6c3b68339f9171af194
Thom van der Velden <T.vanderVelden-1@student.tudelft.nl>
2024-08-27 10:50:57 +0200

    Eerste opdracht

---
+++ Opdrachten/Herhaling/cijfer.py
@@ -0,0 +1,11 @@
+# Het programma vraagt om een toetscijfer (float voor kommagetallen!), zorg dat je programma checkt dat het een geldig toetscijfer is! 
+# (dus bijv. geen 11 of een -1)
+
+# Als het een geldig cijfer is print dan `dankjewel`. Als het een ongeldig cijfer, print dan `Dat is geen geldig cijfer...`
+
+# Als het cijfer boven de 8 is, print dan `Wow, dat is heel hoog`.
+# Als het cijfer onder de 8 maar boven de 5.5 is, print dan `Prima, een voldoende!`.
+# Als het cijfer onder de 5.5 maar boven de 3 is, print dan `Kon slechter maar mag wel beter de volgende keer...`.
+# Als het cijfer onder de 3 is, print dan `Wat ging hier mis?`.
+
+toetscijfer = float(input("Geef een cijfer:"))
