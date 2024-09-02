e11a4b016ee005f564bd4ae6b5caf7f12b90ac9c
Thom van der Velden <T.vanderVelden-1@student.tudelft.nl>
2024-09-02 13:20:47 +0200

    Create gevorderd.py

---
+++ Opdrachten/Herhaling/gevorderd.py
@@ -0,0 +1,5 @@
+# Als je klaar bent met alle 6 de opdrachten kan je aan de slag met deze opdracht
+# De opdracht is als volgt: maak een game die in de console werkt.
+# Dit kan van alles zijn maar je kan bijvoorbeeld proberen om een balletje naar beneden te laten vallen
+# Dit balletje moet je vervolgens oppakken met een platform
+# Je mag het zo gek maken als je zelf wilt, succes!
