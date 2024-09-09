a6666b63d98ee9b038635c0a53ec92080634bc9f
Thom van der Velden <T.vanderVelden-1@student.tudelft.nl>
2024-09-09 15:10:14 +0200

    Update les4.py

--- Opdrachten/PyGame/Les4/les4.py
+++ Opdrachten/PyGame/Les4/les4.py
@@ -1,3 +1,24 @@
+'''
+Gegeven is een weg en een auto. Van de auto is al een rectangle gemaakt en deze gebruiken we om de auto te plaatsen en te laten bewegen.
+
+Van de weg is echter nog geen rectangle gemaakt.
+
+Doe het volgende:
+
+  - Maak een rectangle van de weg met surface.get_rect()
+  - Zorg dat de center van deze rectangle in het midden van de game staat (weg precies in het midden)
+  - Zorg dat de onderkant van de auto (bottom) op dezelfde plek staat als de onderkant van de weg (dan rijdt de auto namelijk op de weg)
+  - Zorg dat wanneer de auto en het obstakel colliden je de auto weer naar het begin van het scherm verplaatst (de pion staat pas op de goede plek als de weg is verplaatst)
+
+Vergeet niet om de weg_surface naar de weg_rect te blitten in plaats van naar een aantal coordinaten!
+
+EXTRA:
+
+Blit een stukje tekst "Game over" naar het scherm als de auto de pion raakt.
+
+Slides: https://docs.google.com/presentation/d/1VjYiTjIcSU_x6R_K0pPASkzf7xis3oa_IpjzcmC-Wq8/edit?usp=sharing
+'''
+
 import pygame
 
 pygame.init()
