ff0908620d823a1fecd1a599a3ce7b895232f7ca
Thom van der Velden <44850442+ThomvanderVelden@users.noreply.github.com>
2024-09-09 11:13:25 +0200

    Update Les2.py

--- Opdrachten/PyGame/Les2/Les2.py
+++ Opdrachten/PyGame/Les2/Les2.py
@@ -5,12 +5,13 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Voetbal game!')
 clock = pygame.time.Clock()
+running = True
 test_font = pygame.font.Font("fonts/horror.ttf", 50)
 
 voetbal_surface = pygame.image.load("graphics/voetbal.png")
 tekst_surface = test_font.render("Voetbal game", False, "green")
 
-while True:
+while running:
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
